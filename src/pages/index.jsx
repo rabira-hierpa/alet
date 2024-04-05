d0bc052efb48eeb950253f66bd61459f128d7b0b
rabra <rabira.hierpa@gmail.com>
2024-04-05 11:35:49 +0300

    Add AboutSection and MissionSection components to landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import "../css/global.css"
 import Hero from "../components/hero"
+import AboutSection from "../components/landing/about"
+import MissionSection from "../components/landing/mission"
 const Index = () => {
   return (
     <Layout>
       <Hero />
+      <AboutSection />
+      <MissionSection />
     </Layout>
   )
 }
